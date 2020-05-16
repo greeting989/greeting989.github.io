338a3fb0a1c16ac199cd2bcffd1b606fce92b2cc
greeting989 <65461110+greeting989@users.noreply.github.com>
2020-05-17 02:12:10 +0530

    Update
    Update

---
+++ greeting989/style1.js
@@ -0,0 +1,4 @@
+var flipcardcontainer = document.querySelector('.flip-card-container');
+flipcardcontainer.addEventListener( 'click', function() {
+    flipcardcontainer.classList.toggle('flip');
+});
